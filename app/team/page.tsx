911e2e9cb775cdfa88541f9645fc834a705a14a1
Garry Trinder <garry@trinder365.co.uk>
2025-02-08 19:09:59 +0000

    feat: Increase badge size in team landing page for better visibility

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -30,7 +30,7 @@ export default async function TeamLandingPage() {
                 {teams.map((team, index) => (
                     <li className="list-group-item d-flex justify-content-between align-items-center" key={`team-${index}`}>
                         <Link key={`team-${team.id}`} href={`team/${team.id}`}>{team.name}</Link>
-                        <Image src={team.badgeUrl ? team.badgeUrl : '/badge.svg'} alt={team.name} width={30} height={30} />
+                        <Image src={team.badgeUrl ? team.badgeUrl : '/badge.svg'} alt={team.name} width={35} height={35} />
                     </li>
                 ))}
             </ul>
