8a7345992a528bf32c42cb99250a73964e809339
Garry Trinder <garry@trinder365.co.uk>
2025-02-17 07:47:50 +0000

    feat: Simplify error message on authentication failure

--- app/error.tsx
+++ app/error.tsx
@@ -5,7 +5,7 @@ export default function ErrorPage() {
         <div className="px-4 py-5 text-center">
             <h1 className="display-5 fw-bold text-primary">Error</h1>
             <div className="col-lg-6 mx-auto">
-                <p className="lead mb-4">An error occurred while trying to authenticate you. Please try again.</p>
+                <p className="lead mb-4">An error occurred.</p>
             </div>
         </div>
     </>
