6d4ab3d5ab4f3da8b2670d5cde93ae8f08ee9195
Garry Trinder <garry@trinder365.co.uk>
2025-03-11 01:38:23 +0000

    feat: Enhance TournamentTable layout with improved styling and team badge display

--- app/season-three/table/table.tsx
+++ app/season-three/table/table.tsx
@@ -16,19 +16,18 @@ interface TableData {
 
 export const TournamentTable = ({ tableData }: { tableData: TableData[] }) => {
     return (
-        <table className="table">
+        <table className="table table-sm">
             <thead>
                 <tr>
-                    <th className="text-secondary">Pos</th>
+                    <th className="text-secondary">#</th>
                     <th className="text-secondary">Team</th>
-                    <th className="text-secondary text-center">P</th>
+                    <th className="text-secondary text-center">PL</th>
                     <th className="text-secondary text-center">W</th>
                     <th className="text-secondary text-center">D</th>
                     <th className="text-secondary text-center">L</th>
-                    <th className="text-secondary text-center d-none d-sm-table-cell">GF</th>
-                    <th className="text-secondary text-center d-none d-sm-table-cell">GA</th>
+                    <th className="text-secondary text-center">+/-</th>
                     <th className="text-secondary text-center">GD</th>
-                    <th className="text-secondary text-center">Pts</th>
+                    <th className="text-secondary text-center">PTS</th>
                 </tr>
             </thead>
             <tbody>
@@ -37,19 +36,16 @@ export const TournamentTable = ({ tableData }: { tableData: TableData[] }) => {
                         <tr key={team.name} className={`${index === 0 ? "text-primary" : ""}`}>
                             <td className={`text-center ${index === 0 ? "text-primary" : ""}`}>{index + 1}</td>
                             <td className={index === 0 ? "text-primary" : ""}>
-                                <span>
-                                    <Image src={team.badgeUrl ? team.badgeUrl : '/badge.svg'} alt={team.name} width={30} height={30} />
-                                </span>
-                                <span className="px-3">
-                                    {team.name}
-                                </span>
+                                <div className="d-flex flex-row gap-2">
+                                    <div><Image src={team.badgeUrl ? team.badgeUrl : '/badge.svg'} alt={team.name} width={30} height={30} /></div>
+                                    <div>{team.name}</div>
+                                </div>
                             </td>
                             <td className={`text-center ${index === 0 ? "text-primary" : ""}`}>{team.played}</td>
                             <td className={`text-center ${index === 0 ? "text-primary" : ""}`}>{team.wins}</td>
                             <td className={`text-center ${index === 0 ? "text-primary" : ""}`}>{team.draws}</td>
                             <td className={`text-center ${index === 0 ? "text-primary" : ""}`}>{team.losses}</td>
-                            <td className={`text-center d-none d-sm-table-cell ${index === 0 ? "text-primary" : ""}`}>{team.goalsFor}</td>
-                            <td className={`text-center d-none d-sm-table-cell ${index === 0 ? "text-primary" : ""}`}>{team.goalsAgainst}</td>
+                            <td className={`text-center ${index === 0 ? "text-primary" : ""}`}>{team.goalsFor}-{team.goalsAgainst}</td>
                             <td className={`text-center ${index === 0 ? "text-primary" : ""}`}>{team.goalsFor - team.goalsAgainst}</td>
                             <td className={`text-center ${index === 0 ? "text-primary" : ""}`}>{team.points}</td>
                         </tr>
