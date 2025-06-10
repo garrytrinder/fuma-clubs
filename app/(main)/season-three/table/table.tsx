b4347b41f468138af1c116ad2a8ad8565fe7d813
Garry Trinder <garry@trinder365.co.uk>
2025-06-10 23:11:54 +0100

    refactor: Improve formatting and structure of TournamentTable component

--- app/(main)/season-three/table/table.tsx
+++ app/(main)/season-three/table/table.tsx
@@ -1,6 +1,6 @@
-'use client';
+"use client";
 
-import Image from 'next/image';
+import Image from "next/image";
 
 export interface TableRow {
   id: number;
@@ -18,43 +18,82 @@ export interface TableRow {
 }
 
 export const TournamentTable = ({ tableData }: { tableData: TableRow[] }) => {
-    return (
-        <table className="table table-sm">
-            <thead>
-                <tr>
-                    <th className="text-secondary text-center">#</th>
-                    <th className="text-secondary">Team</th>
-                    <th className="text-secondary text-center">PL</th>
-                    <th className="text-secondary text-center">W</th>
-                    <th className="text-secondary text-center">D</th>
-                    <th className="text-secondary text-center">L</th>
-                    <th className="text-secondary text-center">+/-</th>
-                    <th className="text-secondary text-center">GD</th>
-                    <th className="text-secondary text-center">PTS</th>
-                </tr>
-            </thead>
-            <tbody>
-                {tableData.map((row, index) => {
-                    return (
-                        <tr key={row.id} className={`${index === 0 ? "text-primary" : ""}`}>
-                            <td className={`text-center ${index === 0 ? "text-primary" : ""}`}>{index + 1}</td>
-                            <td className={index === 0 ? "text-primary" : ""}>
-                                <div className="d-flex flex-row gap-2">
-                                    <div><Image src={row.badgeurl ? row.badgeurl : '/badge.svg'} alt={row.team} width={30} height={30} /></div>
-                                    <div>{row.team}</div>
-                                </div>
-                            </td>
-                            <td className={`text-center ${index === 0 ? "text-primary" : ""}`}>{row.matches_played}</td>
-                            <td className={`text-center ${index === 0 ? "text-primary" : ""}`}>{row.wins}</td>
-                            <td className={`text-center ${index === 0 ? "text-primary" : ""}`}>{row.draws}</td>
-                            <td className={`text-center ${index === 0 ? "text-primary" : ""}`}>{row.losses}</td>
-                            <td className={`text-center ${index === 0 ? "text-primary" : ""}`}>{row.goals_scored}-{row.goals_conceded}</td>
-                            <td className={`text-center ${index === 0 ? "text-primary" : ""}`}>{row.goal_difference}</td>
-                            <td className={`text-center ${index === 0 ? "text-primary" : ""}`}>{row.total_points}</td>
-                        </tr>
-                    );
-                })}
-            </tbody>
-        </table>
-    )
-}
+  return (
+    <table className="table table-sm">
+      <thead>
+        <tr>
+          <th className="text-secondary text-center">#</th>
+          <th className="text-secondary">Team</th>
+          <th className="text-secondary text-center">PL</th>
+          <th className="text-secondary text-center">W</th>
+          <th className="text-secondary text-center">D</th>
+          <th className="text-secondary text-center">L</th>
+          <th className="text-secondary text-center">+/-</th>
+          <th className="text-secondary text-center">GD</th>
+          <th className="text-secondary text-center">PTS</th>
+        </tr>
+      </thead>
+      <tbody>
+        {tableData.map((row, index) => {
+          return (
+            <tr key={row.id} className={`${index === 0 ? "text-primary" : ""}`}>
+              <td
+                className={`text-center ${index === 0 ? "text-primary" : ""}`}
+              >
+                {index + 1}
+              </td>
+              <td className={index === 0 ? "text-primary" : ""}>
+                <div className="d-flex flex-row gap-2">
+                  <div>
+                    <Image
+                      src={row.badgeurl ? row.badgeurl : "/badge.svg"}
+                      alt={row.team}
+                      width={30}
+                      height={30}
+                    />
+                  </div>
+                  <div>{row.team}</div>
+                </div>
+              </td>
+              <td
+                className={`text-center ${index === 0 ? "text-primary" : ""}`}
+              >
+                {row.matches_played}
+              </td>
+              <td
+                className={`text-center ${index === 0 ? "text-primary" : ""}`}
+              >
+                {row.wins}
+              </td>
+              <td
+                className={`text-center ${index === 0 ? "text-primary" : ""}`}
+              >
+                {row.draws}
+              </td>
+              <td
+                className={`text-center ${index === 0 ? "text-primary" : ""}`}
+              >
+                {row.losses}
+              </td>
+              <td
+                className={`text-center ${index === 0 ? "text-primary" : ""}`}
+              >
+                {row.goals_scored}-{row.goals_conceded}
+              </td>
+              <td
+                className={`text-center ${index === 0 ? "text-primary" : ""}`}
+              >
+                {row.goal_difference}
+              </td>
+              <td
+                className={`text-center ${index === 0 ? "text-primary" : ""}`}
+              >
+                {row.total_points}
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
